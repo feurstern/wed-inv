d16dcd899dc4ae897d74d1ea6e83e656e9032685
feurstern <naziincolour123@gmail.com>
2025-11-16 12:15:47 +0700

    #1 modified  some components

---
+++ src/config/config.js
@@ -0,0 +1,57 @@
+// Nanti convert ke Bahasa Indonesia
+const config = {
+  data: {
+    title: "Wedding Rio & Anisa", // Nanti ini otomatis
+    description:
+      "We wil get married and we would like to invite you to our special moment .", // Nanti ini dibikin random
+    groomName: "Rio",
+    brideName: "Anisa",
+    c: "Bapak Groom & Ibu Groom",
+    parentBride: "Bapak Bride & Ibu Bride",
+    date: "2025-11-22",
+    maps_url: "https://maps.app.goo.gl/K6Z11rVxaVxt1fUA8",
+    maps_embed:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.1937475176537!2d106.99105127499058!3d-6.2381753937501125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698da4085f6bb5%3A0x7b66b23d785315ad!2sSAMEDJA!5e0!3m2!1sen!2sid!4v1763265520741!5m2!1sen!2sid",
+    time: "08:00 - 12:30 WIB",
+    location: "Restaurant Samedja",
+    address:
+      " Jl. Ahmad Yani No.10, Marga Jaya, Kec. Bekasi Sel., Kota Bks, Jawa Barat 17141",
+    ogImage: "/images/og-image.jpg",
+    favicon: "/images/favicon.ico",
+    agenda: [
+      {
+        title: "Marriage Settlement",
+        date: "2025-11-22",
+        startTime: "09:00",
+        endTime: "10:30",
+        location: "KUA Bekasi Utara",
+        address:
+          "Jl. KH. Muchtar Tabrani, RT.002/RW.012, Perwira, Bekasi Utara, Bekasi, West Java 17142",
+      },
+      {
+        title: "Lunch Buffet & Celebration",
+        date: "2024-12-24",
+        startTime: "16:16",
+        endTime: "17:30",
+        location: "Restaurant Samedja",
+        address:
+          " Jl. Ahmad Yani No.10, Marga Jaya, Kec. Bekasi Sel., Kota Bks, Jawa Barat 17141",
+      },
+    ],
+    audio: {
+      src: "/audio/fulfilling-humming.mp3", // or /audio/nature-sound.mp3
+      title: "Fulfilling Humming", // or Nature Sound
+      autoplay: true,
+      loop: true,
+    },
+    banks: [
+      {
+        bank: "Bank Central Asia",
+        accountNumber: "5211435667",
+        accountName: "SITI ANISA DIVIANI",
+      }
+    ],
+  },
+};
+
+export default config;
